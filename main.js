3345d84f45f592c11ecc115eb147af82707db205
Andrea Bellome <andrea.bellome@gmail.com>
2023-04-17 10:21:37 +0200

    js1, js2

---
+++ main.js
@@ -0,0 +1,116 @@
+
+/* JSnack 1: */
+const bike = [{
+
+    name: 'Name1',
+    weight: 10
+
+    },
+    {
+        name: 'Name2',
+        weight: 15
+    },
+    {
+        name: 'Name3',
+        weight: 5
+    },
+    {
+        name: 'Name4',
+        weight: 20
+    }
+];
+
+/* extract weight of bikes */
+let arrBike = [];
+for (let i = 0; i < bike.length; i++){
+    arrBike.push(bike[i]['weight']);
+}
+
+/* start: function to find index of min value in array */
+function findMinIndex(arr) {
+    let minIndex = 0;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[minIndex]) {
+        minIndex = i;
+      }
+    }
+    return minIndex;
+}
+/* end: function to find index of min value in array */
+
+/* start: create an array of different random numbers */
+function randArray(min, max, dim){
+    let randArr = [];
+    if (dim > max){
+        console.log( 'dimension of the array should be less than max. value' ); 
+    } else {
+        for (let i = 0; i < dim; i++){
+            let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            while (randArr.includes(randomNumber)) {
+                randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+            randArr.push(randomNumber);
+        }
+    }
+    return randArr;
+}
+/* end: create an array of different random numbers */
+
+let idxMinW = findMinIndex(arrBike);
+const {name, weight} = bike[idxMinW];
+console.log(`La bici più leggera si chiama ${name} e pesa ${weight} kg`)
+
+/* JSnack 2: */
+const teams = [{
+        teamname: 'Name1',
+        fouls: 0,
+        points: 0
+    },
+    {
+        teamname: 'Name2',
+        fouls: 0,
+        points: 0
+    },
+    {
+        teamname: 'Name3',
+        fouls: 0,
+        points: 0
+    },
+    {
+        teamname: 'Name4',
+        fouls: 0,
+        points: 0
+    },
+    {
+        teamname: 'Name5',
+        fouls: 0,
+        points: 0
+    },
+    {
+        teamname: 'Name6',
+        fouls: 0,
+        points: 0
+    }
+];
+
+let minPts = 0;
+let maxPts = 100;
+let minFouls = 0;
+let maxFouls = 30;
+for ( let i = 0; i < teams.length; i++){
+    teams[i]['points'] = randArray(minPts, maxPts, 1);
+    teams[i]['fouls'] = randArray(minPts, maxPts, 1);
+}
+
+let arrNames = [];
+let arrfouls = [];
+for ( let i = 0; i < teams.length; i++ ){
+    arrNames.push( teams[i]['teamname'] );
+    arrfouls.push( teams[i]['fouls'] );
+}
+console.log(arrNames, arrfouls)
+
+
+/* JSnack 3: */
+let arr = ['alberto', 'michele','angelo','simone'];
+
